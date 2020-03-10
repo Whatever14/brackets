c1e64a2c412b1b6becdcbc95b57e54aa6f475a91
Whatever14 <novikov.grsu@gmail.com>
2020-03-10 15:09:20 +0300

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let checker = false;
+ 
+  while(checker === false) {
+    
+    let symbolCounter = str.length;
+ 
+    for(i = 0; i < bracketsConfig.length; i++ ) {
+      str = str.replace(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`, '');
+    }
+
+    if(str.length === symbolCounter) {
+      checker = true;// = dead while cycle
+    }
+  }
+ 
+  if(str.length === 0) {
+    checker = true;
+  } else {
+    checker = false;
+  }
+ 
+  return checker;
 }
